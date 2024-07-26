9ba23a37c87e94b91bc34df4c6adbecb19773a51
Karina Balagazova <karina.balagazova@ackee.cz>
2024-07-26 14:24:16 +0200

    ✨ Cleanup, adding logo and readme

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
-# Configuru Extension
+<div align="center">
 
-This is the README for the `configuruhelper` extension.
+<img src="./resources/logo.png" height="170"/>
+
+# Configuru - VS Code Extension
+
+VS Code extension for the [Configuru](https://github.com/AckeeCZ/configuru) library.
+
+</div>
 
 ## Features
 
-This extension is a helper for the `configuru` library. It provides: 
-- suggestions of name variables for the `config.ts` file from `env.jsonc` file
-- if the `config.ts` file contains a variable that is not present in the `env.jsonc` file, the extension will underline it as an error.
+### Suggestions of name variables
+The extension suggests the names of the variables that are present in the `env.jsonc` file.
+![Example](./resources/suggestions.gif)
+
+### Highlighting of invalid variables
+If your `config.ts` file contains a variable that is not present in the `env.jsonc` file, the extension will underline it as an error.
+![Example](./resources/error_highlighting.png)
 
 **Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,17 @@
 import * as vscode from 'vscode'
 import path = require('path')
 import * as jsonParser from 'jsonc-parser'
-import * as ts from 'typescript'
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+// This method is called when extension is activated
+// Extension is activated the very first time the command is executed
 let diagnosticCollection: vscode.DiagnosticCollection
 export function activate(context: vscode.ExtensionContext) {
-  // TODO read env file on file opening and compare to what is in file and give a warning in case of missing envs
-  // TODO Refresh comparison on every change in file
-
   let parsedDotEnv: any = {}
-  let parsedConfig: any = {}
   diagnosticCollection =
     vscode.languages.createDiagnosticCollection('typescript') // for wave underline
-  // vscode.window.onDidChangeActiveTextEditor(async (file) => {
   vscode.workspace.onDidChangeTextDocument(async file => {
     // TODO do filename configurable by user
     if (file?.document?.fileName.endsWith('/config.ts')) {
-      await vscode.window.showInformationMessage('config.ts file opened') // TODO remove this line -- this is only for debugging
-
       // ============== Read .env.jsonc file ==============
       if (!vscode.workspace.workspaceFolders) {
         return
@@ -29,7 +21,6 @@ export function activate(context: vscode.ExtensionContext) {
       const fileUri = folderUri.with({
         path: path.posix.join(folderUri.path, '.env.jsonc'),
       })
-
       const readData = await vscode.workspace.fs.readFile(fileUri)
       const readStr = Buffer.from(readData).toString('utf8')
 
@@ -40,15 +31,8 @@ export function activate(context: vscode.ExtensionContext) {
       // ============== Read config.ts file ==============
       const text = file.document.getText()
       const pattern = /\([\n\r\s]*('|").*("|')[\n\r\s]*\)/g
-      // // get all keys from configStr by pattern
       const configTsKeys =
         text.match(pattern)?.map(x => x.slice(1, -1).trim().slice(1, -1)) ?? []
-
-      // Update configTsKeys so that it contains only what's inside quotes
-      console.log(configTsKeys)
-
-      // find all differences between parsedDotEnvKeys and configTsKeys
-      // const missingKeysInConfigTs = parsedDotEnvKeys.filter(x => !configTsKeys.includes());
       const missingKeysInDotEnv = configTsKeys.filter(
         x => !parsedDotEnvKeys.includes(x)
       )
@@ -81,22 +65,37 @@ export function activate(context: vscode.ExtensionContext) {
           comments &&
           commentRanges.some(commentRange => commentRange.contains(keyRange))
         if (!isMissingKeyInTheComment) {
-          diagnostics.push(
-            new vscode.Diagnostic(
-              keyRange,
-              `Key ${key} is missing in your .env`,
-              vscode.DiagnosticSeverity.Error
-            )
+          // Underline missing key in config.ts file. Add a message to go to .env.jsonc file and link it
+          const errorMessage = new vscode.Diagnostic(
+            keyRange,
+            `Key '${key}' is missing in .env.jsonc`,
+            vscode.DiagnosticSeverity.Error
           )
+          errorMessage.relatedInformation = [
+            {
+              location: new vscode.Location(file.document.uri, keyRange),
+              message: 'Missing key in .env',
+            },
+            {
+              location: new vscode.Location(
+                fileUri,
+                new vscode.Range(0, 0, 0, 0)
+              ),
+              message: 'This file is missing the key',
+            },
+          ]
+          errorMessage.code = {
+            value: 'missing-key',
+            target: fileUri,
+          }
+          errorMessage.source = 'configuru'
+
+          diagnostics.push(errorMessage)
         }
       }
       diagnosticCollection.set(file.document.uri, diagnostics)
     }
   })
-
-  // const patternCustom = vscode.workspace.workspaceFolders
-  //     ? new vscode.RelativePattern(vscode.workspace.workspaceFolders?.[0], '*.ts')
-  //     : "**/config.ts"
 
   // ======================== Add suggestions to the editor ========================
   context.subscriptions.push(
@@ -113,9 +112,12 @@ export function activate(context: vscode.ExtensionContext) {
             .text.slice(0, position.character)
 
           // TODO better? /loader\.(((string|bool|number|json)\((?:"|'))|(custom\(.*\)\(('|")))$/g;
-          // TODO loader.bool(''''''''''''''''' should not be matched
-          const pattern = /\([\n\r\s]*('|")/g // (' or ("
-          if (!linePrefix.match(pattern)) {
+          // TODO besides \n\r\s also consider comments
+          const pattern = /\([\n\r\s]*('|")/g // (' or (" or divided with spaces/newlines
+          if (
+            !linePrefix.match(pattern) &&
+            !matchMultipleLines(document, position, 5, linePrefix, pattern)
+          ) {
             return undefined
           }
 
@@ -136,7 +138,25 @@ export function activate(context: vscode.ExtensionContext) {
   )
 }
 
-// This method is called when your extension is deactivated
+const matchMultipleLines = (
+  document: vscode.TextDocument,
+  position: vscode.Position,
+  maxLines: number,
+  linePrefix: string,
+  pattern: RegExp
+) => {
+  let isMatching = false
+  let lastLine = position.line - 1
+  while (!isMatching && lastLine > position.line - maxLines && lastLine >= 0) {
+    const line = document.lineAt(lastLine)
+    linePrefix = line.text + linePrefix
+    isMatching = linePrefix.match(pattern) !== null
+    lastLine--
+  }
+  return isMatching
+}
+
+// This method is called when extension is deactivated
 export function deactivate() {
   if (diagnosticCollection) {
     diagnosticCollection.clear()
